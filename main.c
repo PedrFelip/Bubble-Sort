2b959868cad7cd3abfca273978b3828912cc693c
Pedro Felipe <159201061+PedrFelip@users.noreply.github.com>
2024-06-07 22:47:52 -0300

    Create main.c
    backup codigo

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void troca(int* a, int* b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void bubble( int arr[], tamanhoarr){
+    int i, j;
+    bool snap;
+    
+    for(i = 0; i < tamanhoarr - 1; i++){
+        snap = false;
+        
+        
+    }
+    
+}
+int main(){
+    printf("Hello World");
+
+    return 0;
+}
