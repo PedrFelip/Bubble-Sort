2cb453cbc05146b872e8da04d00e1850de71194d
Pedro Felipe <159201061+PedrFelip@users.noreply.github.com>
2024-06-07 23:13:40 -0300

    Update main.c
    codigo completo

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 #include <stdbool.h>
 
 void troca(int* a, int* b){
-    int temp = a;
-    a = b;
-    b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void bubble( int arr[], tamanhoarr){
+void bubbleSort( int arr[], int tamanhoarr){
     int i, j;
     bool snap;
     
@@ -16,7 +16,7 @@ void bubble( int arr[], tamanhoarr){
         snap = false;
         for(j = 0; j < tamanhoarr - i - 1; j++){
             if (arr[j] > arr[j + 1]){
-                troca(arr[j], arr[j + 1]);
+                troca(&arr[j], &arr[j + 1]);
                 snap = true;
             }
         }
@@ -27,8 +27,18 @@ void bubble( int arr[], tamanhoarr){
     }
     
 }
-int main(){
-    printf("Hello World");
+void printArray(int arr[], int size){
+    int i;
+    for(i = 0; i < size; i++){
+        printf("%d ", arr[i]);
+    }
+}
 
+int main(){
+    int arr[] = { 23, 34, 15, 80, 12, 11, 49 };
+    int tamanhoarr = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, tamanhoarr);
+    printf("Bubble array: \n");
+    printArray(arr, tamanhoarr);
     return 0;
 }
